50d7e04650fc041c4efc526b5a765c31e4d432ba
Alexander <degacth@yandex.ru>
2015-08-31 20:23:41 +0300

    ADD admin edit link

--- src/common/templatetags/common.py
+++ src/common/templatetags/common.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 from django import template
 from django.conf import locale
+from django.core.urlresolvers import reverse
 from globals import globals
 
 register = template.Library()
@@ -12,3 +13,12 @@ def active_path_class(cls, path): return cls if globals.request.path.startswith(
 
 @register.filter
 def equals_path_class(cls, path): return cls if globals.request.path == path else ""
+
+
+@register.simple_tag
+def admin_edit(obj):
+    app, model = (obj._meta.app_label, obj._meta.model_name,)
+    if globals.request.user.is_authenticated():
+        return '<a href="%s" target="_blank"><i class="uk-icon-edit"></i></a>' % \
+               reverse("admin:%s_%s_change" % (app, model), args=(obj.id,))
+    return ""
