1125fb73bdb8d7f5a1eef079cb136bc631369341
Alexander <degacth@yandex.ru>
2015-09-27 20:11:25 +0300

    ADD sitename config

--- fixtures/config.py
+++ fixtures/config.py
@@ -3,14 +3,8 @@
 import init
 from config.models import Config
 
-Config.objects.all().delete()
-
-for name in ['title', 'description', 'keywords']:
-    tp = "seo_%s" % name
-    conf = Config(title=tp, name=tp, value="Hello, World")
-    conf.save()
-
-Config(title="Tелефоны", name="phones", value="+7 911 921 83 88,+7 911 834 83 71", type="array").save()
-Config(title="Email адреса", name="emails", value="degacth@yandex.ru,hello@world.ru", type="array").save()
-Config(title="Email адреса admins", name="admin_emails", value="degacth@yandex.ru", type="array").save()
-Config(title="Количество записей на страницу", name="per_page", value="24", type="integer").save()
+Config.objects.create(**{
+    'title': 'Имя сайта',
+    'name': 'site_name',
+    'value': 'NatShop',
+})
